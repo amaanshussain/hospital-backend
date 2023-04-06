324e973b5f84ca77288e25de91379edf7bc6e77c
Amaan Hussain <amaan6307@gmail.com>
2023-04-06 03:57:03 -0500

    converted to sqlmanager

---
+++ SQLManager.js
@@ -0,0 +1,35 @@
+var sql = require('mysql')
+require('dotenv').config() // load .env file into process
+
+function createSQLConnection() {
+    // return a sql connection from values in .env file
+    return sql.createConnection({
+        host: process.env.HOST,
+        port: process.env.PORT,
+        user: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
+    });
+}
+function getSQLQuery(connection, query, callback) {
+    // execute a sql query and send result as response to the caller
+    connection.query(query, function (error, results, fields) {
+        if (error) {
+            callback(error)
+        }
+        let parsedQuery = Object.values(JSON.parse(JSON.stringify(results)));
+        connection.end()
+        callback(parsedQuery)
+    });
+}
+function executeSQLQuery(connection, query, callback) {
+    // execute a sql query and send result as response to the caller
+    connection.query(query, function (error, results, fields) {
+        if (error) {
+            callback(error)
+        }
+        callback(results)
+    })
+}
+
+module.exports = { createSQLConnection, getSQLQuery, executeSQLQuery };
